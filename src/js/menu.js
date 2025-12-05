cbc3e919f915f41f5d4fca586677b204c9349e35
TarasBilyi <taras.bilyi@gmail.com>
2025-12-05 21:39:31 +0200

    edit js

--- src/js/menu.js
+++ src/js/menu.js
@@ -11,6 +11,13 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  const menuLinks = refs.modal.querySelectorAll('a');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      refs.modal.classList.remove('is-open');
+    });
+  });
+
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
